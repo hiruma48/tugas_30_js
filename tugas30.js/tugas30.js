8a7ef054545b365edf2798425d6a3956bb1800db
tri ariyanto <triyanto129@gmail.com>
2023-05-27 16:55:40 +0700

    tugas 30 js
    mengumpulkan tugas js

---
+++ tugas30.js/tugas30.js
@@ -0,0 +1,6 @@
+function Regex(value) {
+    var ambilData = value.match(/[es]/g);
+    console.log(ambilData);
+   }
+   
+   Regex("Para tetua adat mendiskusikan masalah kejahatan yang terjadi di rumah adat");
